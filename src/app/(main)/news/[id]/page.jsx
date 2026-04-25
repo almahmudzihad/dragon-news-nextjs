40b70d23ebc7c95055d80e4ddf8516448b0b7e0d
almahmudzihad <amzihad@gmail.com>
2026-04-25 21:02:57 +0600

    news detels

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,8 +1,71 @@
+import { getNewsDetelsById } from '@/lib/data';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import { FaBookmark, FaEye, FaStar } from 'react-icons/fa';
+import { FaSquareShareNodes } from 'react-icons/fa6';
 
-
-function NewsDetels() {
+const NewsDetels = async({params}) => {
+  const {id} = await params;
+  const newsdata = await getNewsDetelsById(id);
+  const news = newsdata[0];
+  console.log(news)
   return (
-    <div>NewsDetels</div>
+    <div className='max-w-4xl mx-auto'>
+    <div className="card bg-white shadow-md border border-gray-100 my-2">
+          <div className="card-body space-y-4">
+            <div className="flex items-center gap-3 bg-gray-50 p-3 rounded-lg">
+              <Image
+                src={news.author?.img}
+                alt="user"
+                height={40}
+                width={40}
+                className="rounded-full"
+              />
+              <div>
+                <p className="font-semibold text-gray-800">{news.author?.name}</p>
+                <p className="text-xs text-gray-500">
+                  {news.author?.published_date}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 ml-auto text-xl text-gray-500">
+                <FaSquareShareNodes />
+                <FaBookmark />
+              </div>
+            </div>
+    
+            <h2 className="card-title text-gray-800">{news.title}</h2>
+    
+            <figure>
+              <Image
+                src={news.image_url}
+                alt={news.title}
+                width={300}
+                height={200}
+                className="w-full rounded-lg"
+                unoptimized
+              />
+            </figure>
+    
+            <div>
+              <p className=" text-gray-600">{news.details}</p>
+              <button className="btn border-blue-500 text-blue-500 mt-2">
+                <Link href={`/category/${news.category_id}`}>More News of this category</Link>
+              </button>
+            </div>
+    
+            <div className="flex items-center w-full border-t py-2 border-gray-100 text-gray-500">
+              <div className="flex items-center gap-2">
+                <FaStar className="text-orange-400" /> {news.rating?.number}
+              </div>
+    
+              <div className="flex items-center gap-2 ml-auto">
+                <FaEye /> {news.total_view}
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
   )
 }
 
--- src/lib/data.js
+++ src/lib/data.js
@@ -9,3 +9,8 @@ export async function getNewsByCategoryId (catagory_id) {
   const data = await res.json()
   return data
 }
+export async function getNewsDetelsById (news_id) {
+  const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+  const data = await res.json()
+  return data.data
+}
