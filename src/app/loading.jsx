cd94541e5b206e2ebb8a27c9f8d2bfea07d58b82
almahmudzihad <amzihad@gmail.com>
2026-04-25 17:05:09 +0600

    global loding

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center h-[60vh]">
+        <span className="loading loading-spinner text-accent"></span>
+    </div>
+  )
+}
+
+export default LoadingPage
