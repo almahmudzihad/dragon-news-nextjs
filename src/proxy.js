db11e3d1687b37c18e1c3b52874e497bb4e482a1
almahmudzihad <amzihad@gmail.com>
2026-04-27 16:20:11 +0600

    ready for deploy

--- src/proxy.js
+++ src/proxy.js
@@ -19,5 +19,5 @@ export async function proxy(request) {
 // export default function proxy(request) { ... }
  
 export const config = {
-  matcher: ['/career', '/about-us']
+  matcher: ['/career', '/news:path*'],
 };
