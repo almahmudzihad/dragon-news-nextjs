17bc6cfbbd94f981a94581dd562c544b827589ec
almahmudzihad <amzihad@gmail.com>
2026-04-27 13:59:40 +0600

    github login auth

--- src/components/HomePage/News/RightSideBar.jsx
+++ src/components/HomePage/News/RightSideBar.jsx
@@ -6,19 +6,33 @@ import { authClient } from "@/lib/auth-client";
 const RightSideBar = () => {
   const handelGoogleSignIn = async () => {
     const data = await authClient.signIn.social({
-    provider: "google",
-  });
-  
+      provider: "google",
+    });
+  };
+  const handelGithubSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
   };
   return (
     <div>
-        <h2 className="font-bold text-lg text-center mb-3">Login Width</h2>
-        <div className="flex flex-col gap-2">
-        <button className="btn border-blue-500 text-blue-500" onClick={handelGoogleSignIn}><FcGoogle /> Login Width Google</button>
-        <button className="btn border-blue-500 text-blue-500"><FaGithub /> Login Width GitHub</button>
-        </div>
+      <h2 className="font-bold text-lg text-center mb-3">Login Width</h2>
+      <div className="flex flex-col gap-2">
+        <button
+          className="btn border-blue-500 text-blue-500"
+          onClick={handelGoogleSignIn}
+        >
+          <FcGoogle /> Login Width Google
+        </button>
+        <button 
+        className="btn border-blue-500 text-blue-500"
+        onClick={handelGithubSignIn}
+        >
+          <FaGithub /> Login Width GitHub
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RightSideBar
+export default RightSideBar;
